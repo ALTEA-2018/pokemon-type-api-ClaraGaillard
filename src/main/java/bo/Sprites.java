4cc7cc5914240c82f62681cf2f7dd2ae8491dec4
Gaillard Clara <gaillardc@b13p8.fil.univ-lille1.fr>
2019-01-28 12:00:29 +0100

    First steps for the project

---
+++ src/main/java/bo/Sprites.java
@@ -0,0 +1,6 @@
+package bo;
+
+public class Sprites {
+    private String back_default;
+    private String front_default;
+}
---
+++ src/main/java/bo/Stats.java
@@ -0,0 +1,8 @@
+package bo;
+
+public class Stats {
+    private Integer speed;
+    private Integer defense;
+    private Integer attack;
+    private Integer hp;
+}
